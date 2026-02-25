717cb2efda38c75fc7fcfbdf053e1fdc29128c50
Yuvaraj S <yuvaraj@zuper.co>
2026-02-26 02:38:49 +0530

    elite_roofing

--- configs/index.ts
+++ configs/index.ts
@@ -1,4 +1,4 @@
-export const COMPANY_NAME = "Saenz Global"
+export const COMPANY_NAME = "Elite Roofing"
 export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions'
 export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy'
 export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/1b7f03b0-d340-4529-a5a7-97d5e0890510"
