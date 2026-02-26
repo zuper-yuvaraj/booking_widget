8ba76db8792fc1e1c537c14fc852113ea14b4ff8
Yuvaraj S <yuvaraj@zuper.co>
2026-02-26 23:33:26 +0530

    aded the right time zone

--- components/booking-confirmation.tsx
+++ components/booking-confirmation.tsx
@@ -9,16 +9,18 @@ interface BookingConfirmationProps {
 }
 
 export default function BookingConfirmation({ formData }: BookingConfirmationProps) {
-  const formatDate = (dateString: string) => {
-    dateString = dateString + " 00:00:00"
-    const date = new Date(dateString)
-    return date.toLocaleDateString("en-US", {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      timeZone: 'America/New_York'
-    })
+   const formatDate = (dateString: string) => {
+    // dateString = dateString + " 00:00:00"
+    // const date = new Date(dateString)
+
+    return dateString
+    // return date.toLocaleDateString("en-US", {
+    //   weekday: "long",
+    //   year: "numeric",
+    //   month: "long",
+    //   day: "numeric",
+    //   timeZone: TIME_ZONE
+    // })
   }
 
   const formatTime = (timeSlot: string) => {
