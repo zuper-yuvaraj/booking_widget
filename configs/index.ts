1ac3eaded58b83c2d2dc373078865fa55911bcb7
Yuvaraj S <yuvaraj@zuper.co>
2026-02-27 00:00:53 +0530

    added the sun elevated

--- configs/index.ts
+++ configs/index.ts
@@ -1,7 +1,7 @@
-export const COMPANY_NAME = "Saenz Global"
+export const COMPANY_NAME = "Sun Elevated"
 export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions'
 export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy'
 export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/1b7f03b0-d340-4529-a5a7-97d5e0890510"
 export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/abafa55b-e62b-4208-a940-09d26721a885"
-export const COMPANY_UUID = "6c8eb7a3-d0e6-4907-a505-5d5148e325d2"
+export const COMPANY_UUID = "38938420-a072-4f21-a6e5-a107960efda1"
 export const TIME_ZONE = "America/Los_Angeles"
