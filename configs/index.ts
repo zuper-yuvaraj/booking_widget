3b31216ac97f155c07404190f2a23b88455df654
Yuvaraj S <yuvaraj@zuper.co>
2026-03-23 16:13:53 -0400

    shield

--- configs/index.ts
+++ configs/index.ts
@@ -1,7 +1,16 @@
-export const COMPANY_NAME = "Sun Elevated"
-export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions'
-export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy'
+export const COMPANY_NAME = "Evans Roofing and Gutters"
+export const TERMS_OF_SERVICE = 'https://www.evansroofingandgutters.com/terms/'
+export const PRIVACY_POLICY = 'https://www.evansroofingandgutters.com/privacy-policy/'
 export const ASSISTED_SCHEDULING_WEBHOOK = ""
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/4924ab25-11e8-4891-99c0-bb1a1e5e1071"
-export const COMPANY_UUID = "38938420-a072-4f21-a6e5-a107960efda1"
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/cc3f4d6e-df1a-4a64-92e6-e92f32b95896"
+export const COMPANY_UUID = "c5860565-d3c1-4c50-bac3-faedc55017ab"
 export const TIME_ZONE = "America/Los_Angeles"
+
+
+// export const COMPANY_NAME = "Evans Roofing and Gutters"
+// export const TERMS_OF_SERVICE = 'https://www.evansroofingandgutters.com/terms/'
+// export const PRIVACY_POLICY = 'https://www.evansroofingandgutters.com/privacy-policy/'
+// export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/f8d289d8-bca1-4411-a1a1-bb1cd7555a40"
+// export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/cc3f4d6e-df1a-4a64-92e6-e92f32b95896"
+// export const COMPANY_UUID = "c5860565-d3c1-4c50-bac3-faedc55017ab"
+// export const TIME_ZONE = "America/Los_Angeles"
