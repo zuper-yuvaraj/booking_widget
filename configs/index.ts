01c9091166dfe5fa38f192c8dfbb8db0ca5fa826
Yuvaraj S <yuvaraj@zuper.co>
2026-03-27 12:23:18 -0400

    whilte roofing

--- configs/index.ts
+++ configs/index.ts
@@ -1,10 +1,10 @@
-export const COMPANY_NAME = "Evans Roofing and Gutters"
-export const TERMS_OF_SERVICE = 'https://www.evansroofingandgutters.com/terms/'
-export const PRIVACY_POLICY = 'https://www.evansroofingandgutters.com/privacy-policy/'
+export const COMPANY_NAME = "White Roofing & Contracting"
+export const TERMS_OF_SERVICE = 'https://ljwhiteroofing.com/terms-and-conditions/'
+export const PRIVACY_POLICY = 'https://ljwhiteroofing.com/privacy-policy/'
 export const ASSISTED_SCHEDULING_WEBHOOK = ""
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/cc3f4d6e-df1a-4a64-92e6-e92f32b95896"
-export const COMPANY_UUID = "c5860565-d3c1-4c50-bac3-faedc55017ab"
-export const TIME_ZONE = "America/Los_Angeles"
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/0c4c3fa0-f8f7-48e4-9a4d-f1babc7dfcbb"
+export const COMPANY_UUID = "89eed610-9754-4cfc-9ab1-881a3cb3bb86"
+export const TIME_ZONE = "Etc/GMT+8"
 
 
 // export const COMPANY_NAME = "Evans Roofing and Gutters"
