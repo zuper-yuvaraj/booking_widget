80e84d0f9b37d457848e3662727cb13a15f36e4e
Yuvaraj S <yuvaraj@zuper.co>
2026-04-15 10:11:34 -0400

    changes to the patriot roofing

--- configs/index.ts
+++ configs/index.ts
@@ -1,16 +1,9 @@
-export const COMPANY_NAME = "White Roofing & Contracting"
-export const TERMS_OF_SERVICE = 'https://ljwhiteroofing.com/terms-and-conditions/'
-export const PRIVACY_POLICY = 'https://ljwhiteroofing.com/privacy-policy/'
+export const COMPANY_NAME = "Patriot Roofing and Restoration"
+export const TERMS_OF_SERVICE = 'https://patriotroof.net/terms-of-service'
+export const PRIVACY_POLICY = 'https://patriotroof.net/privacy-policy/'
 export const ASSISTED_SCHEDULING_WEBHOOK = ""
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/0c4c3fa0-f8f7-48e4-9a4d-f1babc7dfcbb"
-export const COMPANY_UUID = "89eed610-9754-4cfc-9ab1-881a3cb3bb86"
-export const TIME_ZONE = "Etc/GMT+8"
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/f5e8610d-0f4a-41b8-a3ea-0cefc9d14930"
+export const COMPANY_UUID = "a12b39b7-970a-4562-875f-d12fbb8dde34"
+export const TIME_ZONE = "America/Los_Angeles"
 
 
-// export const COMPANY_NAME = "Evans Roofing and Gutters"
-// export const TERMS_OF_SERVICE = 'https://www.evansroofingandgutters.com/terms/'
-// export const PRIVACY_POLICY = 'https://www.evansroofingandgutters.com/privacy-policy/'
-// export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/f8d289d8-bca1-4411-a1a1-bb1cd7555a40"
-// export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/cc3f4d6e-df1a-4a64-92e6-e92f32b95896"
-// export const COMPANY_UUID = "c5860565-d3c1-4c50-bac3-faedc55017ab"
-// export const TIME_ZONE = "America/Los_Angeles"
