46af2260b4115088df9869ffa799c184d9854787
Yuvaraj S <yuvaraj@zuper.co>
2026-04-24 12:59:43 -0700

    chnages

--- configs/index.ts
+++ configs/index.ts
@@ -1,6 +1,6 @@
 export const COMPANY_NAME = "HYTEK"
-export const TERMS_OF_SERVICE = ''
-export const PRIVACY_POLICY = ''
+export const TERMS_OF_SERVICE = 'https://www.hytekco.com/privacy-policy/'
+export const PRIVACY_POLICY = 'https://www.hytekco.com/privacy-policy/'
 export const ASSISTED_SCHEDULING_WEBHOOK = ""
 export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/9dfe8954-b8fc-4f00-a3d3-fee90fad8708"
 export const COMPANY_UUID = "f2c4f3b6-b576-48f9-b601-5e8d9ed40138"
