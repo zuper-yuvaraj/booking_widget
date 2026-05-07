313a5602bdacdf427a274e01868605a6a48656cb
Ananthula Sharanya <sharanya@zuper.co>
2026-05-07 09:05:49 -0700

    Pushed Default lead qualification with preferred date

--- configs/index.ts
+++ configs/index.ts
@@ -1,5 +1,12 @@
-export const COMPANY_NAME = "Zuper Roofing"
-export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions'
-export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy'
-export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/fda86828-8a7a-4df9-8d23-e1283b12531c"
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/906256cc-c15b-4b65-ac6d-0230c10c035d"
+export const COMPANY_NAME = "" // Company Name
+
+export const PRIVACY_POLICY = '' // Company Privacy Policy URL
+
+export const TERMS_OF_SERVICE = '' // Company Terms of Service URL
+
+export const CREATE_BOOKING_WEBHOOK = "" // Webhook URL to create booking
+
+export const COMPANY_ID = "" // Company UID
+
+export const Timezone = "" // Company Timezone
+
