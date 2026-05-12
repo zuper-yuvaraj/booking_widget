0a2d43911c939eb38ea1679da9fa2b2e9e65f8c0
Yuvaraj S <yuvaraj@zuper.co>
2026-05-12 14:13:09 -0500

    changes

--- configs/index.ts
+++ configs/index.ts
@@ -1,9 +1,9 @@
-export const COMPANY_NAME = "SC Roofing LLC"
+export const COMPANY_NAME = ""
 export const TERMS_OF_SERVICE = ''
 export const PRIVACY_POLICY = ''
-export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/5b66f69b-5a40-4847-ab65-77eb5a6b9857"
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/3f55a937-2f31-4e19-a9fa-524aa08381e6"
-export const COMPANY_UUID = "c6915471-d2e6-40ff-95e3-8f5071739dac"
-export const TIME_ZONE = "America/Chicago"
+export const ASSISTED_SCHEDULING_WEBHOOK = ""
+export const CREATE_BOOKING_WEBHOOK = ""
+export const COMPANY_UUID = ""
+export const TIME_ZONE = "" //America/Chicago
 
 
