a405b9ae0d876baea1f38c8038e1b50841424118
Yuvaraj S <yuvaraj@zuper.co>
2026-05-14 08:42:53 -0500

    test

--- configs/index.ts
+++ configs/index.ts
@@ -1,9 +1,9 @@
-export const COMPANY_NAME = ""
-export const TERMS_OF_SERVICE = ''
-export const PRIVACY_POLICY = ''
-export const ASSISTED_SCHEDULING_WEBHOOK = ""
-export const CREATE_BOOKING_WEBHOOK = ""
-export const COMPANY_UUID = ""
-export const TIME_ZONE = "" //America/Chicago
+export const COMPANY_NAME = "Slot View Demo"
+export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions'
+export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy'
+export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/cb505c38-763a-437f-a1de-1ff2a8e2bb53"
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/6256feba-b716-42af-8a22-12047a246523"
+export const COMPANY_UUID = "c6dcd594-a537-4fae-b7b2-fcf77c722c8c"
+export const TIME_ZONE = "America/Los_Angeles" //America/Chicago
 
 
