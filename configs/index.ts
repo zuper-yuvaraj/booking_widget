956098f4fab9a6c50cca4a85e8f63b397f863669
Ananthula Sharanya <sharanya@zuper.co>
2026-05-15 10:26:42 -0700

    test lq default

--- configs/index.ts
+++ configs/index.ts
@@ -1,8 +1,8 @@
-export const COMPANY_NAME = "" // Company name
+export const COMPANY_NAME = "Booking Widget" // Company name
 
-export const PRIVACY_POLICY = '' // Company privacy policy url
-export const TERMS_OF_SERVICE = '' // Company terms and conditions url
+export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy' // Company privacy policy url
+export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions' // Company terms and conditions url
 
-export const CREATE_BOOKING_WEBHOOK = ""; // Webhook URL to create booking
-export const COMPANY_UID = ""; // Company UID
-export const timezone = ""; // Company Timezone
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/732488d5-fc21-4936-ac86-050101b34070"; // Webhook URL to create booking
+export const COMPANY_UID = "c6dcd594-a537-4fae-b7b2-fcf77c722c8c"; // Company UID
+export const timezone = "America/Los_Angeles"; // Company Timezone
