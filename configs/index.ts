6d9135abae8226e83d9325ed35590dddae662a1e
Yuvaraj S <yuvaraj@zuper.co>
2026-05-29 11:27:11 -0700

    changes

--- configs/index.ts
+++ configs/index.ts
@@ -1,9 +1,9 @@
-export const COMPANY_NAME = ""
-export const TERMS_OF_SERVICE = ''
-export const PRIVACY_POLICY = ''
-export const ASSISTED_SCHEDULING_WEBHOOK = ""
-export const CREATE_BOOKING_WEBHOOK = ""
-export const COMPANY_UUID = ""
-export const TIME_ZONE = "" //America/Chicago
+export const COMPANY_NAME = "Single Roofing Golden Account"
+export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions' 
+export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy'
+export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/204c6c0a-7ac6-4ec8-94d7-dfe40e44b578"
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/fef33df6-7821-47af-b29b-5402a1c7c9ec"
+export const COMPANY_UUID ="4b89535b-e118-47a9-8415-c528b7dbc4d4" 
+export const TIME_ZONE = "America/New_York"//America/Chicago
 
 
