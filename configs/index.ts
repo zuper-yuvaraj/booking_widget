30b99ba429bb64a49d0fb91d229d7da2cdea388d
Yuvaraj S <yuvaraj@zuper.co>
2026-05-29 10:44:08 -0700

    single_roofing_RG3

--- configs/index.ts
+++ configs/index.ts
@@ -1,12 +1,12 @@
-export const COMPANY_NAME = "CasaBella Roofing" // Company Name
+export const COMPANY_NAME = "Single Roofing Golden Account" // Company Name
 
-export const PRIVACY_POLICY = 'https://casabellaroofing.com/privacy-policy/' // Company Privacy Policy URL
+export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy' // Company Privacy Policy URL
 
-export const TERMS_OF_SERVICE = 'https://casabellaroofing.com/terms-and-conditions/' // Company Terms of Service URL
+export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions' // Company Terms of Service URL
 
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/fd1f9562-5434-4f4c-8df1-8f715a3b4131" // Webhook URL to create booking
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/dd7f71d5-a76d-49aa-b5bb-d6ebe865681f" // Webhook URL to create booking
 
-export const COMPANY_ID = "f94a40d8-480b-4b40-9519-5d0740f74874" // Company UID
+export const COMPANY_ID = "4b89535b-e118-47a9-8415-c528b7dbc4d4" // Company UID
 
-export const Timezone = "America/Los_Angeles" // Company Timezone
+export const Timezone = "America/New_York" // Company Timezone
 
