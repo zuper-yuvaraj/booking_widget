8cdb4036051f4f920c9d0ff75b2da69c245039aa
Yuvaraj S <yuvaraj@zuper.co>
2026-05-29 11:39:37 -0700

    changes

--- configs/index.ts
+++ configs/index.ts
@@ -1,10 +1,10 @@
-export const COMPANY_NAME = "" // Company name
-export const TERMS_OF_SERVICE = '' // Terms of service URL
-export const PRIVACY_POLICY = '' // Privacy policy URL
-export const ASSISTED_SCHEDULING_WEBHOOK = "" //n8n assisted scheduling webhook URL
-export const CREATE_BOOKING_WEBHOOK = "" //n8n create booking webhook URL
-export const COMPANY_UUID = "" // Zuper Company UUID, used for n8n webhooks to identify the company
-export const TIME_ZONE = "" // Time zone for the application, e.g., "America/New_York"
+export const COMPANY_NAME = "Single Roofing Golden Account" // Company name
+export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions'// Terms of service URL
+export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy' // Privacy policy URL
+export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/326323ef-e44f-4710-b2ea-10b4fd21eac8" //n8n assisted scheduling webhook URL
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/e9845254-8711-4269-b9e3-eedf65f0b225" //n8n create booking webhook URL
+export const COMPANY_UUID = "4b89535b-e118-47a9-8415-c528b7dbc4d4" // Zuper Company UUID, used for n8n webhooks to identify the company
+export const TIME_ZONE = "America/New_York" // Time zone for the application, e.g., "America/New_York"
 
 
 // template url - https://internalwf.zuper.co/workflow/2Hi1DuyOvVCLbLQ0
