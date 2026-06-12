4aa4a083776b1b6690a6a1029d8ab6dd47130cbe
Ananthula Sharanya <sharanya@zuper.co>
2026-06-12 11:09:00 -0500

    Pushed bergens roofing lead qualification preferred date

--- configs/index.ts
+++ configs/index.ts
@@ -1,5 +1,7 @@
-export const COMPANY_NAME = "Zuper Roofing"
-export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions'
-export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy'
-export const ASSISTED_SCHEDULING_WEBHOOK = "https://internalwf.zuper.co/webhook/fda86828-8a7a-4df9-8d23-e1283b12531c"
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/906256cc-c15b-4b65-ac6d-0230c10c035d"
+export const COMPANY_NAME = "Bergens Roofing"
+
+export const PRIVACY_POLICY = 'https://bergensroofing.com/privacy-policy/'
+
+export const TERMS_OF_SERVICE = 'https://bergensroofing.com/terms-of-service'
+
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/2f5e4dfd-25df-4db1-b1fd-6a3b4b051242"
