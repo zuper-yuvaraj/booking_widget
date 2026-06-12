48c486970688c3d67c30fac6f720d15e3130f7fd
Ananthula Sharanya <sharanya@zuper.co>
2026-06-12 12:15:47 -0500

    Pushed Thanveer Roofing Booking widget

--- configs/index.ts
+++ configs/index.ts
@@ -1,8 +1,8 @@
-export const COMPANY_NAME = "" // Company name
+export const COMPANY_NAME = "Thanveer Roofing" // Company name
 
-export const PRIVACY_POLICY = '' // Company privacy policy url
-export const TERMS_OF_SERVICE = '' // Company terms and conditions url
+export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy' // Company privacy policy url
+export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions' // Company terms and conditions url
 
-export const CREATE_BOOKING_WEBHOOK = ""; // Webhook URL to create booking
-export const COMPANY_UID = ""; // Company UID
-export const timezone = ""; // Company Timezone
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/514afdf4-bee6-4a56-bc6d-04f6b66b1a75"; // Webhook URL to create booking
+export const COMPANY_UID = "81e6983b-b158-4ca8-91c7-26e81780403b"; // Company UID
+export const timezone = "America/New_York"; // Company Timezone
