eddf786ce39a74a0c73a1767519bafb4c38c571c
Yuvaraj S <yuvaraj@zuper.co>
2026-07-27 16:24:17 +0530

    OO1-T7_one_oak

--- configs/index.ts
+++ configs/index.ts
@@ -1,9 +1,9 @@
-export const COMPANY_NAME = "Patriot Roofing and Restoration"
-export const TERMS_OF_SERVICE = 'https://patriotroof.net/terms-of-service'
-export const PRIVACY_POLICY = 'https://patriotroof.net/privacy-policy/'
+export const COMPANY_NAME = "One Oak Exteriors"
+export const TERMS_OF_SERVICE = 'https://oneoakexteriors.com/terms-of-service/'
+export const PRIVACY_POLICY = 'https://oneoakexteriors.com/privacy-policy/'
 export const ASSISTED_SCHEDULING_WEBHOOK = ""
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/f5e8610d-0f4a-41b8-a3ea-0cefc9d14930"
-export const COMPANY_UUID = "a12b39b7-970a-4562-875f-d12fbb8dde34"
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/127eb634-27c3-41e4-ba4f-0135f8394aef"
+export const COMPANY_UUID = "e9d08437-c3ce-463f-9cca-956d17498ab1"
 export const TIME_ZONE = "America/Los_Angeles"
 
 
