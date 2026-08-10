24af7c12848bd0b5a106b0ce65936f4b3cd9ff65
Varshini <varshini@zuper.co>
2026-08-10 08:45:39 -0500

    changes_to_step_2

--- configs/index.ts
+++ configs/index.ts
@@ -1,5 +1,5 @@
 export const COMPANY_NAME = "Shield Roofing"
-export const TERMS_OF_SERVICE = 'http://shieldroofrepair.com/privacy-policy/'
+export const TERMS_OF_SERVICE = 'https://www.shieldroofrepair.com/terms-and-conditions/'
 export const PRIVACY_POLICY = 'http://shieldroofrepair.com/privacy-policy/'
 export const ASSISTED_SCHEDULING_WEBHOOK = ""
 export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/4eefd7c7-d2da-4a8e-9eec-8c056dd9f3d4"
