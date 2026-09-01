662e1c8715493fbf23b3176cdbd53f1ef4524a06
Ananthula Sharanya <sharanya@zuper.co>
2026-09-01 12:48:19 -0700

    Created account for Ferguson_and_Melvin_Roofing_Lead_Qualification_PreferredDate

--- configs/index.ts
+++ configs/index.ts
@@ -1,8 +1,8 @@
-export const COMPANY_NAME = "Booking Widget" // Company name
+export const COMPANY_NAME = "Ferguson & Melvin Roofing Co." // Company name
 
-export const PRIVACY_POLICY = 'https://www.zuper.co/privacy-policy' // Company privacy policy url
-export const TERMS_OF_SERVICE = 'https://www.zuper.co/terms-conditions' // Company terms and conditions url
+export const PRIVACY_POLICY = 'https://lp.fmroof.com/privacypolicyv2' // Company privacy policy url
+export const TERMS_OF_SERVICE = 'https://lp.fmroof.com/terms_of_service' // Company terms and conditions url
 
-export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/c69bab43-ceb0-46e8-8f92-6e7fbcc5e01c"; // Webhook URL to create booking
-export const COMPANY_UID = "c6dcd594-a537-4fae-b7b2-fcf77c722c8c"; // Company UID
+export const CREATE_BOOKING_WEBHOOK = "https://internalwf.zuper.co/webhook/d636ec34-e91c-449a-ac6f-88f43e784503"; // Webhook URL to create booking
+export const COMPANY_UID = "05f7681c-bd1e-4f2b-8833-110758e79f13"; // Company UID
 export const timezone = "America/Los_Angeles"; // Company Timezone
